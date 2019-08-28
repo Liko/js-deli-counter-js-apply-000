78900c393f008a6884616fa679a3edb5e54f93d5
Li Xie <li_xie1992@hotmail.com>
2019-08-28 12:12:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(currentLine, name) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0){
-    return `Currently serving ${katzDeliline.shift()}.`
+    return `Currently serving ${katzDeliLine.shift()}.`
   } else if (katzDeliLine.length == 0){
     return "There is nobody waiting to be served!"
   }
