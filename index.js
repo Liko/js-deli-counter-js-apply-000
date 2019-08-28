92814eacb33bbe2175110f423665b7fc420f628c
Li Xie <li_xie1992@hotmail.com>
2019-08-28 12:06:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(currentLine, name) {
-  return `Welcome, ${name}. You are number ${currentLine+1} in the line.
+  return `Welcome, ${name}. You are number ${currentLine+1} in the line`.
     
   }
